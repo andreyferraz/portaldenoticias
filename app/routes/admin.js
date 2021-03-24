e500824aafac559a9bea74ae28b1484cf547d10f
Andrêy Ferraz <andrey.developer@hotmail.com>
2021-03-23 21:23:17 -0300

    Efetuado a validação dos campos do formulário de inclusão

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,6 +7,21 @@ module.exports = function(app){
     app.post('/noticias/salvar', function(req, res){
         const noticia = req.body 
 
+        // tratamento de validação dos campos dos formulários 
+        req.assert('titulo', 'Título é obrigatório').notEmpty()
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty()
+        req.assert('resumo', 'Resumo deve conter de 10 a 100 caracteres').len(10, 100)
+        req.assert('autor', 'Autor é obrigatório').notEmpty()
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'})
+        req.assert('noticia', 'Noticia é obrigatório').notEmpty()
+
+        const erros = req.validationErrors()
+
+        if(erros){
+            res.render("admin/form_add_noticia")
+            return
+        }
+
         const connection = app.config.dbConnection()
         const noticiasModel = new app.app.models.NoticiasDAO(connection)
 
